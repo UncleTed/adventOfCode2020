f88dd292e2253398803b380b6b9a8976333bf48a
TLAY <tlayher@gmail.com>
2020-12-10 07:45:07 -0500

    TLAY learned a new trick called itertools.combinations

--- day9/day9.py
+++ day9/day9.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
-
+import itertools
+# brute force means of producing sum of two numbers
 def two_of_five(numbers):
     result = []
     j = 0
@@ -14,6 +15,13 @@ def two_of_five(numbers):
         j += 1
     return result
 
+# alternate means of producing sum of two numbers
+def combo(numbers):
+    result = []
+    for c in itertools.combinations(numbers, 2):
+        result.append(c[0] + c[1])
+    return result
+
 def unit_test_sum_of_any_two(actual):
     expected = [55,50,60,82, 35,45,67,40,62,72]
     for i, n in enumerate(expected):
@@ -26,7 +34,7 @@ def part1(input):
     ptr = 25
     # unit_test_sum_of_any_two(input[ptr-5:ptr])
     while True:
-        if input[ptr] not in two_of_five(input[ptr-25:ptr]):
+        if input[ptr] not in combo(input[ptr-25:ptr]):
             print(f'part1: {input[ptr]}')
             break
         ptr += 1
@@ -34,6 +42,7 @@ def part1(input):
 def part2(input):
     # 128499090 is too low
     # 133664652 is too low
+    # 171265123
     # 1212510616 is too high
     part1_answer = 1212510616
     a = 0
